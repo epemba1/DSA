04bdd86613ed6c4c0bd4e5de25e4d260fe0f9df4
epemba1 <eric-younous.pemba1@univ-lyon2.fr>
2024-05-01 18:33:27 +0200

    Update binary-search.js
    Description
    
    Co-Authored-By: dandjee23 <168657988+dandjee23@users.noreply.github.com>

--- binary-search.js
+++ binary-search.js
@@ -1,18 +1,18 @@
 var binarySearch = function(nums, target) {
     let left = 0;
     let right = nums.length - 1;
-    
+
     while (left <= right) {
         const mid = Math.floor((left + right) / 2);
-        
+
         if (nums[mid] === target) {
-            return mid; // Found the target
+            return mid;
         } else if (nums[mid] < target) {
-            left = mid + 1; // Search the right half
+            left = mid + 1;
         } else {
-            right = mid - 1; // Search the left half
+            right = mid - 1;
         }
     }
-    
-    return -1; // Target not found
+
+    return -1; 
 };
