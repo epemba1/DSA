5db3e20c3fdd16c8a03c85902ebe3399cd2a7611
epemba1 <145719061+epemba1@users.noreply.github.com>
2024-05-15 06:53:28 +0200

    Create numberOfEmployeesWhoMetTarget.js

---
+++ numberOfEmployeesWhoMetTarget.js
@@ -0,0 +1,9 @@
+var numberOfEmployeesWhoMetTarget = function(hours, target) {
+    let result = 0;
+    for(let i = 0; i < hours.length; i++) {
+        if (hours[i] >= target) {
+            result++;
+        }
+    }
+    return result;
+};
