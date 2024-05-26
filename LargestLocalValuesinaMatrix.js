a95ec3b8b363474c4b8713b6b47bf036c53bc560
epemba1 <145719061+epemba1@users.noreply.github.com>
2024-05-26 13:28:59 +0200

    Create LargestLocalValuesinaMatrix.js

---
+++ LargestLocalValuesinaMatrix.js
@@ -0,0 +1,17 @@
+var largestLocal = function(grid) {
+    const n = grid.length;
+    const maxLocal = Array.from({ length: n - 2}, () => Array(n - 2).fill(0));
+
+    for(let i = 0; i < n - 2; i++) {
+        for (let j = 0; j < n - 2; j++) {
+            let maxVal = 0;
+            for (let k = 0; k < 3; k++) {
+                for (let l = 0; l < 3; l++) {
+                    maxVal = Math.max(maxVal, grid[i + k][j + l]);
+                }
+            }
+            maxLocal[i][j] = maxVal;
+        }
+    }
+    return maxLocal;
+};
