9b4e05b5d495cc6ae6c2c2659dbddbee8f427b18
epemba1 <145719061+epemba1@users.noreply.github.com>
2024-06-30 11:37:01 +0200

    Create sumOfSquares.js

---
+++ sumOfSquares.js
@@ -0,0 +1,12 @@
+var sumOfSquares = function(nums) {
+    let sum = 0;
+    const n = nums.length;
+    
+    for (let i = 1; i <= n; i++) {
+        if (n % i === 0) {
+            sum += nums[i-1] * nums[i-1];
+        }
+    }
+    
+    return sum;
+};
